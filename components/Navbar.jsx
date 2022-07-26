8fbd8ba726fa6e16f85119ca4d54d2b5b66c8f59
kemalbekcan <kemalbekcan@gmail.com>
2022-07-26 23:40:27 +0300

    Create Navbar components

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,51 +1,103 @@
-import React from 'react'
+import React from "react";
+import { AiFillPhone } from "react-icons/ai";
+import { AiOutlineMail } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineWechat } from "react-icons/ai";
+import { AiOutlineHeart } from "react-icons/ai";
+import { AiOutlinePlusCircle } from "react-icons/ai";
+import Logo from "../assets/Logo.png";
+import Image from "next/image";
 
 function Navbar() {
   return (
     <nav>
-      <ul>
-        <li>
-          <a href="">phone</a>
+      <ul className="top-navbar flex list-none">
+        <li className="navbar-list">
+          <a className="item" href="">
+            <AiFillPhone />
+            507 797 83 14
+          </a>
         </li>
-        <li>
-          <a href="">gmail</a>
+        <li className="navbar-list">
+          <a className="item" href="">
+            <AiOutlineMail />
+            kirva@gmail.com
+          </a>
         </li>
-        <li>
-          <a href="">Logo</a>
+        <li className="navbar-list">
+          <a className="item" href="">
+            <Image
+              src={Logo}
+              alt="Picture of the author"
+              width={200}
+              height={200}
+            />
+          </a>
         </li>
-        <li>
-          <input type="text" placeholder='Arama Yap' />
+        <li className="navbar-list">
+          <input type="text" placeholder="Arama Yap" />
         </li>
-        <li>
-          <a href="">Profile</a>
+        <li className="navbar-list">
+          <a href="">
+            <AiOutlineUser />
+          </a>
         </li>
-        <li>
-          <a href="">Cart</a>
+        <li className="navbar-list">
+          <a href="">
+            <AiOutlineShoppingCart />
+          </a>
         </li>
-        <li>
-          <a href="">Heart</a>
+        <li className="navbar-list">
+          <a href="">
+            <AiOutlineHeart />
+          </a>
         </li>
-        <li>
-          <a href="">Chat</a>
+        <li className="navbar-list">
+          <a href="">
+            <AiOutlineWechat />
+          </a>
         </li>
-        <li>
-          <a href="">Plus</a>
+        <li className="navbar-list">
+          <a href="">
+            <AiOutlinePlusCircle />
+          </a>
         </li>
       </ul>
-      <ul>
-        <li>Teknoloji</li>
-        <li>Spor Malzemeleri</li>
-        <li>Kutu Oyunları</li>
-        <li>Giyim</li>
-        <li>Enstürman</li>
-        <li>Stüdyo Ekipmanları</li>
-        <li>Hırdavat</li>
-        <li>Outdoor</li>
-        <li>Düğün Gelinlik</li>
-        <li>Emlak</li>
+      <ul className="navbar flex list-none">
+        <li>
+          <a href="">Teknoloji</a>
+        </li>
+        <li>
+          <a href="">Spor Malzemeleri</a>
+        </li>
+        <li>
+          <a href="">Kutu Oyunları</a>
+        </li>
+        <li>
+          <a href="">Giyim</a>
+        </li>
+        <li>
+          <a href="">Enstürman</a>
+        </li>
+        <li>
+          <a href="">Stüdyo Ekipmanları</a>
+        </li>
+        <li>
+          <a href="">Hırdavat</a>
+        </li>
+        <li>
+          <a href="">Outdoor</a>
+        </li>
+        <li>
+          <a href="">Düğün Gelinlik</a>
+        </li>
+        <li>
+          <a href="">Emlak</a>
+        </li>
       </ul>
     </nav>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
